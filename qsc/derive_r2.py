9bb53e131c6a58de1b4f452675ffe5cc2548cfd8
Semo Zitouni <semozitouni@icloud.com>
2024-12-06 12:32:02 -0600

    setting up skeleton for derive r2

--- qsc/derive_r2.py
+++ qsc/derive_r2.py
@@ -59,7 +59,7 @@ def derive_B20(rc, zs, rs=[], zc=[], nfp=1, etabar=1., sigma0=0., B0=1., I2=0., 
   
   X1c = derive_calc_X1c(etabar, nphi, nfp, rc, rs, zc, zs)
 
-  Y1c = derive_calc_Y1c(sG, spsi, nphi, nfp, rc, rs, zc, zs, sigma, etabar)
+  Y1c = derive_calc_Y1c(sG, spsi, nphi, nfp, rc, rs, zc, zs, sigma, etabar) # relies on newtons 
 
   Y1s = derive_calc_Y1s(sG, spsi, nphi, nfp, rc, rs, zc, zs, etabar)
 
@@ -69,7 +69,7 @@ def derive_B20(rc, zs, rs=[], zc=[], nfp=1, etabar=1., sigma0=0., B0=1., I2=0., 
 
   mu0 =  4 * jnp.pi * 1e-7
 
-  qc = calc_qc(iota_N, X1c, Y1s, torsion, abs_G0_over_B0)
+  qc = calc_qc(iota_N, X1c, Y1s, torsion, abs_G0_over_B0) #
   qs = calc_qs(iota_N, X1c, Y1s, torsion, abs_G0_over_B0)
 
   rc = calc_rc(d_d_varphi, Y1c, iota_N, Y1s, X1c, torsion, abs_G0_over_B0)
@@ -92,11 +92,17 @@ def derive_B20_mean(nphi, nfp, rc, rs, zc, zs):
   return jnp.sum(B20 * d_l_d_phi) * normalizer
 
 def derive_B20_anomaly(nphi, nfp, rc, rs, zc, zs):
+  """
+  calculates B20 anomaly as a function of inputed parameters
+  """
   B20_mean = derive_B20_mean(nphi, nfp, rc, rs, zc, zs) 
   B20 = derive_B20()
   return B20 - B20_mean
 
 def derive_B20_residual(B0, nphi, nfp, rc, rs, zc, zs): 
+  """
+  calculates B20 residual as a fucntion of inputed parameters 
+  """
   B20 = derive_B20()
   B20_mean = derive_B20_mean(nphi, nfp, rc, rs, zc, zs)
   d_l_d_phi = calc_d_l_d_phi(nphi, nfp, rc, rs, zc, zs)
@@ -105,48 +111,139 @@ def derive_B20_residual(B0, nphi, nfp, rc, rs, zc, zs):
   return jnp.sqrt(jnp.sum((B20 - B20_mean) * (B20 - B20_mean) * d_l_d_phi) * normalizer) / B0
 
 def derive_B20_variation(): 
+  """
+  calcultes B20_variation as a function of inputed parameters
+  """
   B20 = derive_B20()
   return jnp.max(B20) - jnp.min(B20)
 
 def derive_N_helicity(nfp): 
+  """
+  calculates N_helicity as a function of inputed parameters
+  """
   helicity = # in init_axis // will take alot of untangaling
   return - helicity * nfp
 
 def derive_G2(B0 ,I2, p2 ,sG ,nphi ,nfp, rc, rs, zc, zs): 
+  """
+  calculates G2 as a function of inputed parameters
+  """
   mu0 = 4 * jnp.pi * 1e-7
   G0 = calc_G0(sG,  nphi,  B0, nfp, rc, rs, zc, zs)
   iota = # solve sigma equation
   return -mu0 * p2 * G0 / (B0 * B0) - iota * I2
 
 def derive_d_curvature_d_varphi(rc, zs, rs, zc, nfp,  nphi): 
+  """
+  calculates d_curvature_d_varphi as a function of inputed parameters
+  """
   d_d_varphi = calc_d_d_varphi(rc, zs, rs, zc, nfp,  nphi)
   curvature = calc_curvature(nphi, nfp, rc, rs, zc, zs)
   jnp.matmul(d_d_varphi, curvature)
 
 def derive_d_torsion_d_varphi(rc, zs, rs, zc, nfp,  nphi): 
+  """
+  calculates d_torsion_d_varphi as a function of inputed parameters
+  """
   d_d_varphi = calc_d_d_varphi(rc, zs, rs, zc, nfp,  nphi)
   torsion = # B20
   return jnp.matmul(d_d_varphi, torsion)
 
 def derive_d_X20_d_varphi(rc, zs, rs, zc, nfp, nphi): 
+  """
+  calculates d_X20_d_varphi as a function of inputed parameters
+  """
   d_d_varphi = calc_d_d_varphi(rc, zs, rs, zc, nfp,  nphi)
   solutions = calc_solutions()
   X20 = calc_X20(solutions, nphi)
   return jnp.matmul(d_d_varphi, X20)
 
 def derive_d_X2s_d_varphi(rc, zs, rs, zc, nfp,  nphi): 
+  """
+  calculates d_X2s_d_varphi as a function of inputed parameters
+  """
   d_d_varphi = calc_d_d_varphi(rc, zs, rs, zc, nfp,  nphi)
   # this will be a pain should probably make a derive calc_X2s
   X2s = calc_X2s(B0_over_abs_G0, d_d_varphi, Z2s, iota_N, Z2c, abs_G0_over_B0, B2s, B0, qc, qs, rc, rs, curvature) 
   return jnp.matmul(d_d_varphi, X2s)
 
 def derive_d_X2c_d_varphi(): 
+  """
+  calculates d_X2c_d_varphi as a function of inputed parameters
+  """
   d_d_varphi = calc_d_d_varphi(rc, zs, rs, zc, nfp,  nphi)
   X2c = calc_X2c()
   return jnp.matmul(d_d_varphi, X2c)
 
 def derive_d_Y20_d_varphi(rc, zs, rs, zc, nfp,  nphi): 
+  """
+  calculates d_Y20_d_varphi as a function of inputed parameters
+  """
   d_d_varphi = calc_d_d_varphi(rc, zs, rs, zc, nfp,  nphi)
   solution = 
   Y20 = solution[nphi:2 * nphi]
   return jnp.matmul(d_d_varphi, Y20)
+
+def derive_d_Y2s_d_varphi():
+  """
+  calculates d_Y2s_d_varphi as a function of inputed parameters
+  """
+  d_d_varphi = 
+  Y2s = 
+  return jnp.matmul(d_d_varphi, Y2s)
+
+def derive_d_Y2c_d_varphi():
+  """
+  calculates d_Y2c_d_varphi as a function of inputed parameters
+  """
+  d_d_varphi = 
+  Y2c =
+  return jnp.matmul(d_d_varphi, Y2c)
+  
+def derive_d_Z20_d_varphi():
+  """
+  calculates d_Z20_d_varphi as a function of inputed parameters
+  """
+  d_d_varphi = 
+  Z20 = 
+  return jnp.matmul(d_d_varphi, Z20)
+
+def derive_d_Z2s_d_varphi():
+  """
+  calculates d_Z2s_d_varphi as a function of inputed parameters
+  """
+  d_d_varphi = 
+  Z2s = 
+  return jnp.matmul(d_d_varphi, Z2s)
+
+def derive_d_Z2c_d_varphi():
+  """
+  calculates d_Z2c_d_varphi as a function of inputed parameters
+  """
+  d_d_varphi = 
+  Z2c = 
+  return jnp.matmul(d_d_varphi, Z2c)
+
+def derive_d2_X1c_d_varphi2(): 
+  """
+  calculates d2_X1c_d_varphi2 as a function of inputed parameters
+  """
+  d_d_varphi = 
+  d_X1c_d_varphi = 
+  return jnp.matmul(d_d_varphi, d_X1c_d_varphi)
+  
+def derive_d2_Y1c_d_varphi2(): 
+  """
+  calculates d2_Y1c_d_varphi2 as a function of inputed parameters
+  """
+  d_d_varphi = 
+  d_Y1c_d_varphi = 
+  return jnp.matmul(d_d_varphi, d_Y1c_d_varphi)
+
+def derive_d2_Y1s_d_varphi2(): 
+  """
+  calculates d2_Y1s_d_varphi2 as a function of inputed parameters
+  """
+  d_d_varphi = 
+  d_Y1s_d_varphi = 
+  return jnp.matmul(d_d_varphi, d_Y1s_d_varphi)
---
+++ qsc/derive_r3.py
@@ -0,0 +1,30 @@
+from .calculate_r3 import *
+from .calculate_r1_helpers import derive_calc_X1c, derive_calc_X1s, derive_calc_Y1c, derive_calc_Y1s
+
+def derive_flux_constraint_coefficient(): 
+
+
+def derive_X3c1(): 
+  # requires X1c & flux_constraint_coefficient
+  X1c = derive_calc_X1c
+  flux_constraint_coefficient = 
+  return X1c * flux_constraint_coefficient
+
+def derive_Y3c1(): 
+
+def derive_Y3s1(): 
+  
+def derive_X3s1(): 
+
+def derive_d_X3c1_d_varphi():
+
+def derive_d_Y3c1_d_varphi(): 
+
+def derive_d_Y3s1_d_varphi(): 
+
+def derive_Q(): 
+
+def derive_predicted_flux_constraint_coefficient(): 
+
+def derive_B0_order_a_squared_to_cancel(): 
+
