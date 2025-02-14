b2eab6450b172ae1e14db33b238c32ff93211da5
Semo Zitouni <semozitouni@icloud.com>
2025-02-14 15:40:24 -0600

    Update simple_demo.py

--- examples/simple_demo.py
+++ examples/simple_demo.py
@@ -62,7 +62,7 @@ print(stel.iota) # Rotational transform on-axis for this configuration
 #print(stel.d2_volume_d_psi2) # Magnetic well V''(psi) Semo: this is calculated in "mercier.py" in pyqsc which does not exist in pyNACX
 #print(stel.DMerc_times_r2) # Mercier criterion parameter DMerc multiplied by r^2 Semo: this is calculated in "mercier.py" in pyqsc which does not exist in pyNACX
 print(stel.min_L_grad_B) # Scale length associated with the grad grad B tensor
-print(stel.grad_grad_B_inverse_scale_length) # Scale length associated with the grad grad B tensor Semo: grad_grad_B_inverse_scale_length is not calculted in r1
+#print(stel.grad_grad_B_inverse_scale_length) # Scale length associated with the grad grad B tensor Semo: grad_grad_B_inverse_scale_length is not calculted in r1
 print("plotting...")
 stel.plot_boundary() # Plot the flux surface shape at the default radius r=1
 stel.plot() # Plot relevant near axis parameters
